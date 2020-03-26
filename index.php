37225f8d5710615c053854e30404e6edf4c4e44b
Gomersonger <Gomersong@gmail.com>
2020-03-26 10:29:14 +0300

    final

--- index.php
+++ index.php
@@ -1,52 +1,54 @@
 <?php
+echo "<h1>№1</h1>";
 //Задача № 1 Вывести на экран N случайных чисел (каждое с новой строки) из диапазона [-21, 35]
 function random_output($counter=0)
 {
-  if($counter<=0){
+  if($counter <= 0){
     echo "Число повторений(N) отрицательное,отсутсвует или равно 0.<br/>";
   }
   else {
     for ($i=0; $i <$counter ; $i++) {
-        echo (rand(-21, 35))."<br/>";
+        echo ( rand(-21, 35) )."<br/>";
     }
   }
 
 }
 
 random_output(10);
-
+echo "<h1>№2</h1>";
 //Задача № 2 Вывести на экран сумму N случайных чисел.
 function random_sum_output()
 {
-  $number1= rand();
-  $number2= rand();
-  $sum = $number1+$number2;
+  $number1 = rand();
+  $number2 = rand();
+  $sum = $number1 + $number2;
   echo $sum."<br/>";
   }
-
 random_sum_output();
 
+
+echo "<h1>№3</h1>";
 //Задача № 3 Вывести на экран N случайных чисел (каждое с новой строки). Для каждого числа, начиная со второго, рядом выводить - больше ли оно предыдущего или меньше.
 
 function random_output_plus($counter=0)
 {
-  if($counter<=0){
-    echo "Число повторений(N) отрицательное,отсутсвует или равно 0.<br/>";
+  if ($counter<=0){
+        echo "Число повторений(N) отрицательное,отсутсвует или равно 0.<br/>";
   }
   else {
     for ($i=0; $i < $counter ; $i++) {
-        $numberIter=rand(-101,101);
-        if($i==0){
-          echo $numberIter." Начальное <br/>";
-          $number_before=$numberIter;
+        $numberIter = rand(-101,101);
+        if ($i==0){
+            echo $numberIter." Начальное <br/>";
+            $number_before = $numberIter;
         }
-        elseif ($numberIter>$number_before) {
+        elseif ($numberIter > $number_before) {
         echo $numberIter." Больше <br/>";
-        $number_before=$numberIter;
+        $number_before = $numberIter;
         }
         else {
         echo $numberIter." Меньше <br/>";
-          $number_before=$numberIter;
+          $number_before = $numberIter;
         }
 
 
@@ -57,23 +59,25 @@ function random_output_plus($counter=0)
 
 random_output_plus(10);
 
+
+echo "<h1>№4</h1>";
 //Задача № 4 Вывести на экран n-ное число Фибоначчи.
 
-function fibonachi_numbers($maxValue=0){
-  if($maxValue>0){
-    $numberF=0;
-    $numberS=1;
-    $counter=0;
+function fibonachi_numbers($maxValue = 0){
+  if($maxValue > 0){
+    $numberF = 0;
+    $numberS = 1;
+    $counter = 0;
     echo $numberF."<br/>";
     echo $numberS."<br/>";
         while ($counter < $maxValue) {
-          $counter=$numberF+$numberS;
-          if($counter>$maxValue){
+          $counter = $numberF + $numberS;
+          if($counter > $maxValue){
             break;
           }else {
             echo $counter."<br/>";
-            $numberF=$numberS;
-            $numberS=$counter;
+            $numberF = $numberS;
+            $numberS = $counter;
           }
         }
       }
@@ -84,40 +88,41 @@ function fibonachi_numbers($maxValue=0){
 fibonachi_numbers(100);
 
 
-
+ echo "<h1>№5</h1>";
  //Задача № 5 Дано некоторое число длиной от 1 до 5 цифр - вывести его цифры в обратном порядке.
 function backNumbers($value=512)
 {
   $calculate = 0;
-  $num=0;
+  $num = 0;
   echo $value." =  <br/>";
   while ($value > 0) {
     $num = $value % 10;
     $value = intdiv($value,10);
-    $calculate= $calculate*10+$num;
+    $calculate = $calculate * 10 + $num;
   }
-  $calculate=$calculate."";
-for ($i=0; $i <iconv_strlen($calculate) ; $i++) {
+  $calculate = $calculate."";
+for ($i=0; $i < iconv_strlen($calculate); $i++) {
   echo $calculate[$i]."<br/>";
 }
 
 }
 backNumbers(2312312);
 
+echo "<h1>№6</h1>";
 //Задача № 6 Дано некоторое число длиной от 1 до 5 символов, вывести все его "нечётные" цифры в обратном порядке
 echo "<br/>";
 
 function backNumbersEven($value=512)
 {
   $calculate = 0;
-  $num=0;
+  $num = 0;
   echo $value." =  <br/>";
   while ($value > 0) {
     $num = $value % 10;
     $value = intdiv($value,10);
-    $calculate= $calculate*10+$num;
+    $calculate = $calculate * 10 + $num;
   }
-  $calculate=$calculate."";
+  $calculate = $calculate."";
   $countNum = 0;
 for ($i = 0; $i < iconv_strlen($calculate); $i++) {
   if ($calculate[$i] % 2 !== 0){
@@ -133,12 +138,13 @@ if ($countNum == 0){
 }
 backNumbersEven(1231231);
 
+ echo "<h1>№7</h1>";
 //Задача №7 Задать массив из 7-ми элементов через предварительное описание типа,
 // заполнить его случайными значениями в одном цикле,
 // а в другом цикле вывести эти значения на экран.
 function buildArray($arraySize = 1){
   $myArray = [];
-  for ($i = 0; $i < $arraySize ; $i++) {
+  for ($i = 0; $i < $arraySize; $i++) {
     $myArray[$i] = rand(1,999);
   }
 
@@ -193,7 +199,7 @@ $Chars = 0;
   for ($j = 0; $j < count($treeArray); $j++) {
     echo $treeArray[$j];
     $count = $count + 1;
-  if($count > $Chars){
+  if ($count > $Chars){
         echo "<br/>";
         $count = 0;
         $Chars += 1;
@@ -218,6 +224,7 @@ $chars = 0;
   }
 }
 fir_tree_numbers(100);
+
 echo "<h1>№10</h1>";
 //Задача №10 Задайте случайным образом массив из N элементов (целых чисел).
 // Найдите максимальное число и напечатайте его.
